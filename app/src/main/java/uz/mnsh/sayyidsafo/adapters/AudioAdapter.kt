b846a565e1ed2cae2e25b6e880fd951dd7ec95c6
dell <IlhomjonIbragimov1502@gmail.com>
2022-04-25 17:10:26 +0500

    adapter update

--- app/src/main/java/uz/mnsh/sayyidsafo/adapters/AudioAdapter.kt
+++ app/src/main/java/uz/mnsh/sayyidsafo/adapters/AudioAdapter.kt
@@ -1,5 +1,6 @@
 package uz.mnsh.sayyidsafo.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,10 +11,11 @@ import uz.mnsh.sayyidsafo.databinding.ItemMusicBinding
 class AudioAdapter(val list: List<Data>) : RecyclerView.Adapter<AudioAdapter.Vh>() {
 
     inner class Vh(var itemRv: ItemMusicBinding) : RecyclerView.ViewHolder(itemRv.root) {
+        @SuppressLint("SetTextI18n")
         fun onBind(audioResponse: Data) {
             itemRv.tvMusicName.text = audioResponse.name
-            itemRv.tvTimer.text = audioResponse.duration
-            itemRv.tvCloud.text = audioResponse.size
+            itemRv.tvTimer.text = millisecondTo(audioResponse.duration.toInt())
+            itemRv.tvCloud.text = kbToMb(audioResponse.size.toDouble()).toString()+" MB"
         }
     }
 
@@ -26,4 +28,17 @@ class AudioAdapter(val list: List<Data>) : RecyclerView.Adapter<AudioAdapter.Vh>
     }
 
     override fun getItemCount(): Int = list.size
+
+    private fun millisecondTo(milliseconds:Int):String{
+        val minutes = milliseconds  / 60
+        val seconds = milliseconds  % 60
+
+        return "$minutes:$seconds"
+    }
+
+    private fun kbToMb(kb:Double):Double{
+        val MB = (kb*10/1024).toInt()
+
+        return MB.toDouble()/10
+    }
 }
